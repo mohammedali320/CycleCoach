2bf6216c2fa283afad06951abf6766fcbe9eaeef
mohammedali230 <mhejiy@gmail.com>
2025-08-03 11:06:52 +0300

    workouts view controller

---
+++ controllers/workouts/viewController.js
@@ -0,0 +1,38 @@
+const RESOURCE_PATH = '/workouts'
+
+const viewController={
+    signUp(req, res, next){
+        res.render('auth/SignUp')
+    },
+    signIn(req, res, next){
+        res.render('auth/SignIn')
+    },
+    index(req, res, next){
+        res.render('/workouts/Index', res.locals.data)
+    },
+    show(req, res, next){
+        res.render('/workouts/Show', res.locals.data)
+    },
+    edit(req, res, next){
+        res.render('/workouts/Edit', res.locals.data)
+    },
+    newView(req, res, next){
+        res.render('/workouts/New', res.locals.data)
+    },
+    redirectHome(req, res, next){
+    if(res.locals.data.token){
+      res.redirect(`${RESOURCE_PATH}?token=${res.locals.data.token}`)
+    }else {
+      res.redirect(RESOURCE_PATH)
+    } 
+  },
+  redirectShow(req, res, next){
+    if(res.locals.data.token){
+      res.redirect(`${RESOURCE_PATH}/${req.params.id}?token=${res.locals.data.token}`)
+    }else {
+      res.redirect(RESOURCE_PATH + `/${req.params.id}`)
+    } 
+  }
+}
+
+module.export=viewController
